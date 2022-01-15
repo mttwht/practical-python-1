32d068f0656787dde32b871e3d89173813ca86bb
Matt <Matthew.White02@gmail.com>
2022-01-15 09:44:14 +0000

    1.9 Extra Payment Calculator Add variables to make configuring easier

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -2,18 +2,24 @@
 #
 # Exercise 1.7
 
+# user variables
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 month = 0
 
+
 while principal > 0:
-    principal = principal * (1 + rate / 12) - payment
-    total_paid = total_paid + payment
     month += 1
-    if month <= 12:
-        principal -= 1000
-        total_paid += 1000
+    monthly_payment = payment
+    if month >= extra_payment_start_month and month <= extra_payment_end_month:
+        monthly_payment += extra_payment
+    principal = principal * (1 + rate / 12) - monthly_payment
+    total_paid = total_paid + monthly_payment
 
 print("Total paid", round(total_paid, 2), "over", month, "months")
