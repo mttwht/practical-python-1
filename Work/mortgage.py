e9f64eb60f29319f9b3c3d7ba7916aa704d58fbd
Matt <Matthew.White02@gmail.com>
2022-01-15 09:30:10 +0000

    ex 1.8 extra payments account for extra payments in the first 12 months

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,9 +6,14 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+month = 0
 
 while principal > 0:
     principal = principal * (1 + rate / 12) - payment
     total_paid = total_paid + payment
+    month += 1
+    if month <= 12:
+        principal -= 1000
+        total_paid += 1000
 
-print("Total paid", total_paid)
+print("Total paid", round(total_paid, 2), "over", month, "months")
