45d5e592108b39aeca445f5c91bf6117319b790e
Matt <Matthew.White02@gmail.com>
2022-01-16 16:29:15 +0000

    1.27 reading a data file Read a data file and perform a calculation on its contents

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,20 @@
 # pcost.py
 #
 # Exercise 1.27
+
+f = open("Data/portfolio.csv", 'rt')
+headers = next(f).split(',')
+
+total_price = 0
+
+for line in f:
+    row = line.split(',')
+    name = row[0]
+    shares = int(row[1])
+    price = float(row[2])
+    total_price += shares * price
+
+f.close()
+
+print("Total cost", round(total_price, 2))
+    
