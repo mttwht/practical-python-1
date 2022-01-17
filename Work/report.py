2fb25f6b83d8acac09cb512a2b5df9a04d6c9add
Matt <Matthew.White02@gmail.com>
2022-01-17 11:31:42 +0000

    2.4 A list of tuples Read the portfolio into a tuple list

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,30 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+import sys
+
+def read_portfolio(filename):
+    'Read a stock portfolio from a file'
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        portfolio = []
+        for row in rows:
+            try:
+                name = row[0]
+                shares = int(row[1])
+                price = float(row[2])
+                holding = (name, shares, price)
+                portfolio.append(holding)
+            except ValueError:
+                print("Could not convert values", row, "to [str, int, float]")
+            
+    return portfolio
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
