297150a5e798bb4bd3ed4b648e1838a7ce50ff1f
Matt <Matthew.White02@gmail.com>
2022-01-17 12:25:38 +0000

    2.7 Finding out if you can retire calculate current stock values and total gain/loss

--- Work/report.py
+++ Work/report.py
@@ -32,17 +32,32 @@ def read_prices(filename):
     'Read stock prices from a file into a dictionary'
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
-        stonks = {}
+        stocks = {}
         for row in rows:
             try:
                 name, price = row[0], float(row[1])
-                stonks[name] = price
+                stocks[name] = price
             except IndexError:
                 print("Not enough values in line:", row)
-    return stonks
+    return stocks
 
 
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
+
+if len(sys.argv) == 3:
+    filename1, filename2 = sys.argv[1], sys.argv[2]
 else:
-    filename = 'Data/portfolio.csv'
+    filename1, filename2 = 'Data/portfolio.csv', 'Data/prices.csv'
+
+portfolio = read_portfolio(filename1)
+prices = read_prices(filename2)
+
+initial_cost = current_cost = 0.0
+
+for holding in portfolio:
+    initial_cost += holding['shares'] * holding['price']
+    current_cost += holding['shares'] * prices[holding['name']]
+
+print('Purchase price:', initial_cost)
+print('Current value:', current_cost)
+
+print('Gain of', current_cost - initial_cost)
