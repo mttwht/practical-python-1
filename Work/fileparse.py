5b4bc9871773ee56c5e24a8ae4d5de218202a195
Matt <Matthew.White02@gmail.com>
2022-01-29 15:04:00 +0000

    3.6: Working without Headers Add a parameter for specifying no headers

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,28 +4,35 @@
 
 import csv
 
-def parse_csv(filename, select=[], types=[]):
+def parse_csv(filename, select=[], types=[], has_headers=True):
     '''
     Parse a CSV file into a list of records
     '''
     with open(filename) as f:
         rows = csv.reader(f)
-        headers = next(rows)
-        # Filter headers if 'select' is provided
-        if select:
-            indexes = [headers.index(s) for s in select]
-            headers = select
+        # Skip headers if not has_headers
+        if has_headers:
+            headers = next(rows)
+            # Filter headers if 'select' is provided
+            if select:
+                indexes = [headers.index(s) for s in select]
+                headers = select
             
         records = []
         for row in rows:
             if not row:
                 continue
             # Filter columns if 'select' is provided
-            if select:
+            if select and has_headers:
                 row = [row[i] for i in indexes]
             # Type convert if 'types' is provided
             if types:
                 row = [func(val) for func, val in zip(types, row)]
-            record = dict(zip(headers, row))
+			# Convert to dict if has_headers
+            if has_headers:
+                record = dict(zip(headers, row))
+            # Convert to tuple if not has_headers
+            else:
+                record = tuple(row)
             records.append(record)
     return records
